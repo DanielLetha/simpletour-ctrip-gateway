74aff8ae624efe3f3d1e70479f24bf3d150b72fe
mario <mth@simpletour.com>
2016-01-26 17:43:53 +0800

    修改携程对接接口

--- src/main/java/com/simpletour/gateway/ctrip/rest/pojo/bo/CtripOrderBo.java
+++ src/main/java/com/simpletour/gateway/ctrip/rest/pojo/bo/CtripOrderBo.java
@@ -93,9 +93,9 @@ public class CtripOrderBo {
         }
         //TODO.....后台的价格和订购数量都是通过查询数据库或者计算得出，这个地方对接时需要注意
         if (!(this.requestBodyType.getPrice() == null || this.requestBodyType.getPrice().isEmpty())) {
-            orderItem.setSourcePrice(BigDecimal.valueOf(Long.parseLong(this.requestBodyType.getPrice())));
+//            orderItem.setSourcePrice(BigDecimal.valueOf(Long.parseLong(this.requestBodyType.getPrice())));
         }
-        orderItem.setQuantity(this.requestBodyType.getCount());
+//        orderItem.setQuantity(this.requestBodyType.getCount());
         try {
             orderItem.setDate(DateUtil.convertStrToDate(this.requestBodyType.getUseDate(), "yyyy-MM-dd"));
         } catch (ParseException e) {
