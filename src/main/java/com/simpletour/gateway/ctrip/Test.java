f41a42ff0d411180d7bbc93325feef9f8cf5ca59
mario <mth@simpletour.com>
2016-03-02 16:27:22 +0800

    修改携程对接接口

--- src/main/java/com/simpletour/gateway/ctrip/Test.java
+++ src/main/java/com/simpletour/gateway/ctrip/Test.java
@@ -1,60 +1,134 @@
 package com.simpletour.gateway.ctrip;
 
 import com.simpletour.common.utils.MD5;
+import com.simpletour.gateway.ctrip.config.SysConfig;
 import com.simpletour.gateway.ctrip.rest.pojo.VerifyOrderRequest;
+import com.simpletour.gateway.ctrip.rest.pojo.type.orderType.ExtendInfoType;
+import com.simpletour.gateway.ctrip.rest.pojo.type.orderType.PassengerInfo;
 import com.simpletour.gateway.ctrip.rest.pojo.type.orderType.RequestBodyType;
 import com.simpletour.gateway.ctrip.rest.pojo.type.RequestHeaderType;
+import com.simpletour.gateway.ctrip.util.StringUtils;
 import com.simpletour.gateway.ctrip.util.XMLParseUtil;
 import sun.misc.BASE64Encoder;
 
 import javax.xml.bind.JAXBException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 /**
  * Created by Jeff.Song on 2015/12/30.
  */
 public class Test {
 
-    public static void main(String[] args) throws JAXBException {
-        //构造body数据
+    public static void main(String[] args) throws JAXBException, UnsupportedEncodingException, InstantiationException, IllegalAccessException {
+//        //构造body数据
+//        RequestBodyType bodyType = new RequestBodyType();
+//        bodyType.setCount(2);
+//        bodyType.setContactName("张三");
+//        String xml = XMLParseUtil.convertToXml(bodyType);
+//        System.out.println(xml);
+//
+//        //去掉xml头信息
+//        int pos = xml.lastIndexOf("?>");
+//        String bodyString = xml.substring(pos + 2, xml.length());
+//        System.out.println(bodyString);
+//
+//        //将body进行base64编码，再和携程要求的数据一起做MD5摘要运算
+//        String xmlBase64 = new BASE64Encoder().encode(bodyString.getBytes());
+//        StringBuilder builder = new StringBuilder();
+//        builder.append("account");
+//        builder.append("verifyOrder");
+//        builder.append("2015-10-19 16:05:31");
+//        builder.append(xmlBase64);
+//        builder.append("2.0");
+//        builder.append("signKey");
+//        String sign = MD5.getMD5String(builder.toString().getBytes());
+//        System.out.println(sign);
+//
+//        RequestHeaderType headerType = new RequestHeaderType();
+//        headerType.setAccountId("account");
+//        headerType.setServiceName("verifyOrder");
+//        headerType.setRequestTime("2015-10-19 16:05:31");
+//        headerType.setSign(sign);
+//        headerType.setVersion("2.0");
+//
+//        //组装最后的数据
+//        VerifyOrderRequest request = new VerifyOrderRequest();
+//        request.setHeader(headerType);
+//        request.setBody(bodyType);
+//        String ret = XMLParseUtil.convertToXml(request);
+//        System.out.println();
+//        System.out.println("-------------打印最后数据--------------");
+//        System.out.println();
+//        System.out.println(ret);
+
+        System.out.println(buildString(SysConfig.CREATE_ORDER_METHOD));
+
+    }
+
+    private static String buildString(String serviceName) throws UnsupportedEncodingException, IllegalAccessException, InstantiationException {
+        //构造数据
+        //1.构造body信息
         RequestBodyType bodyType = new RequestBodyType();
+        ExtendInfoType extendInfoType = new ExtendInfoType();
+        extendInfoType.setProductType("0");
+        bodyType.setExtendInfo(extendInfoType);
+        bodyType.setProductId("471");
+        bodyType.setPrice("1.00");
         bodyType.setCount(2);
-        bodyType.setContactName("张三");
-        String xml = XMLParseUtil.convertToXml(bodyType);
-        System.out.println(xml);
+        bodyType.setContactName("偏分偏出三分");
+        bodyType.setContactMobile("130111111111");
+        bodyType.setUseDate("2016-02-29");
+        bodyType.setUseEndDate("2016-02-29");
+        bodyType.setOtaOrderId("2340002978-343264" + Random.class.newInstance().nextInt());
 
-        //去掉xml头信息
-        int pos = xml.lastIndexOf("?>");
-        String bodyString = xml.substring(pos + 2, xml.length());
-        System.out.println(bodyString);
+        List<PassengerInfo> passengerInfos = new ArrayList<>();
+        PassengerInfo passengerInfo = new PassengerInfo();
+        passengerInfo.setName("马大叔");
+        passengerInfo.setMobile("18516232422");
+        passengerInfo.setCardType("1");
+        passengerInfo.setCardNo("511102199107200011");
+        passengerInfos.add(passengerInfo);
 
-        //将body进行base64编码，再和携程要求的数据一起做MD5摘要运算
-        String xmlBase64 = new BASE64Encoder().encode(bodyString.getBytes());
-        StringBuilder builder = new StringBuilder();
-        builder.append("account");
-        builder.append("verifyOrder");
-        builder.append("2015-10-19 16:05:31");
-        builder.append(xmlBase64);
-        builder.append("2.0");
-        builder.append("signKey");
-        String sign = MD5.getMD5String(builder.toString().getBytes());
-        System.out.println(sign);
+        PassengerInfo passengerInfo1 = new PassengerInfo();
+        passengerInfo1.setName("马大叔他大叔");
+        passengerInfo1.setMobile("18516232422");
+        passengerInfo1.setCardType("1");
+        passengerInfo1.setCardNo("511102199107200011");
+        passengerInfos.add(passengerInfo1);
+        bodyType.setPassengerInfos(passengerInfos);
 
+
+        //2.构造header信息
         RequestHeaderType headerType = new RequestHeaderType();
-        headerType.setAccountId("account");
-        headerType.setServiceName("verifyOrder");
-        headerType.setRequestTime("2015-10-19 16:05:31");
-        headerType.setSign(sign);
+        headerType.setAccountId("71");
+        headerType.setServiceName(serviceName);
+        headerType.setRequestTime("2016-02-29 16:54:52");
         headerType.setVersion("2.0");
 
         //组装最后的数据
         VerifyOrderRequest request = new VerifyOrderRequest();
         request.setHeader(headerType);
         request.setBody(bodyType);
-        String ret = XMLParseUtil.convertToXml(request);
-        System.out.println();
-        System.out.println("-------------打印最后数据--------------");
-        System.out.println();
-        System.out.println(ret);
 
+        String xml = XMLParseUtil.convertToXml(request);
+        String xmlBodyString = StringUtils.formatXml(XMLParseUtil.subBodyStringForXml(xml));
+
+        //3.编码sign
+        String xmlBase64 = org.bouncycastle.util.encoders.Base64.toBase64String(xmlBodyString.getBytes("UTF-8"));
+        StringBuffer buffer = new StringBuffer();
+        buffer.append(headerType.getAccountId());
+        buffer.append(headerType.getServiceName());
+        buffer.append(headerType.getRequestTime());
+        buffer.append(xmlBase64);
+        buffer.append(headerType.getVersion());
+        buffer.append("9C1012E99067AA970A972103B2CD3D0C");
+        String sign = MD5.getMD5String(buffer.toString().getBytes()).toLowerCase();
+        headerType.setSign(sign);
+
+        return XMLParseUtil.convertToXml(request);
     }
+
 }
